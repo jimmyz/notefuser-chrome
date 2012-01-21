20780d99175c620ca3020d0e92287e99441ed3d5
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2012-01-21 07:19:18 -0700

    First commit.

---
+++ notefuser_familysearch.js
@@ -0,0 +1,87 @@
+// $('.splitFatherLink').html('Yo Dawg!');
+var times = 0;
+var load_checker = window.setInterval(check_nav,4000);
+
+function create_evn_link() {
+  $('#tablist').append("<li><a href=\"#\" id=\"evernoteNav\">NoteFuser</a></li>");
+}
+
+function check_nav(){
+  navigation = $('#tablist');
+  evernote_link = $('#evernoteNav');
+  if(navigation.length > 0 && evernote_link.length == 0 ){
+    create_evn_link();
+  }
+}
+
+function get_focused_id () {
+  main_url = $('a#main').attr('href');
+  expr = /[A-Z0-9]{4}-[A-Z0-9]{3}/;
+  id = main_url.match(expr)[0];
+  return id;
+}
+
+function notefuser_template(){
+  template = "<div id=\"note_fuser\">" +
+    "<h1>NoteFuser</h1>"+
+    "<div><a href=\"#\" id=\"create_research_log\">Create a Research Log</a></div>"+
+    "<div id=\"notefuser_alerts\"></div>"+
+    "<div id=\"notefuser_notes\">Loading...</div>"+
+  "</div>";
+  return template;
+}
+
+function load_notes() {
+  $('#notefuser_notes').html('Loading...');
+  focused_id = get_focused_id();
+  $.getJSON('https://new.familysearch.org/familytree/v2/person/'+focused_id+'?assertions=none&personas=all&dataFormat=application/json',function(famtree){
+    ids = $.map(famtree.persons[0].personas.persona,function(persona){
+      return 'fs.'+persona.id;
+    });
+    $.get('http://notefuser.herokuapp.com/tags/search?tags='+ids.join(','),function(data){
+      $('#notefuser_notes').html(data);
+    });
+  });
+}
+
+function load_notefuser_div(){
+  $('#tablist a.activeOption').removeClass('activeOption');
+  $('#evernoteNav').addClass('activeOption');
+  $('#indiDetails').html(notefuser_template());
+  load_notes();
+  return false;
+}
+
+function create_research_log() {
+  focused_id = get_focused_id();
+  $.getJSON('https://new.familysearch.org/familytree/v2/person/'+focused_id+'?dataFormat=application/json',function(famtree){
+    person = famtree.persons[0]
+    id = 'fs.'+ person.id
+    name = person.assertions.names[0].value.forms[0].fullText
+    console.log(name);
+    if(person.assertions.events[0] && person.assertions.events[0].value.type == 'Birth' &&person.assertions.events[0].value.place){
+      place = person.assertions.events[0].value.place
+      if(place.normalized){
+        locality = place.normalized.value;
+      }else{
+        locality = place.original;
+      }
+    }else{
+      locality = '';
+    }
+    $('#notefuser_notes').html('Loading...')
+    $.post('http://notefuser.herokuapp.com/notes',{id: id, name: name, locality: locality, type: 'log'},function(data){
+      $('#notefuser_alerts').html(data);
+      load_notes();
+    });
+  });
+  return false;
+}
+
+$('#evernoteNav').live('click',load_notefuser_div);
+$('#create_research_log').live('click',create_research_log);
+$('.show_evernote').live('click',function(){
+  guid = $(this).attr('rel');
+  $('#note'+guid).toggle();
+  return false;
+});
---
+++ notefuser_geni.js
@@ -0,0 +1,95 @@
+function create_nf_link() {
+  $('#revision_tab').before("<li id=\"noteFuserTab\"><a href=\"#/tab/notefuser\" id=\"noteFuser\">NoteFuser</a></li>");
+  $('#discussion_tab_content').after(notefuser_template());
+}
+
+function get_focused_id () {
+  path = window.location.pathname;
+  expr = /people\/[^/]*\/([0-9]*)/;
+  id = path.match(expr)[1];
+  return 'geni.'+id;
+}
+
+
+function setup_tab_views () {
+  $('#overview_tab_content').addClass('other_tab_content');
+  $('#media_tab_content').addClass('other_tab_content');
+  $('#timeline_tab_content').addClass('other_tab_content');
+  $('#discussion_tab_content').addClass('other_tab_content');
+  $('#source_tab_content').addClass('other_tab_content');
+  $('#revision_tab_content').addClass('other_tab_content');
+}
+
+function hide_other_tabs () {
+  $('.other_tab_content').hide();
+}
+
+function load_notes() {
+  $('#notefuser_notes').html('Loading...');  
+  focused_id = get_focused_id();
+  $.get('http://notefuser.herokuapp.com/tags/search?tags='+focused_id+'&notes_only=true',function(data){
+    $('#notefuser_notes').html(data);
+  });
+}
+
+function load_notefuser_div(){
+  $('#profile_tabs li.current').removeClass('current');
+  $('#profile_tabs li.first_current').removeClass('first_current');
+  $('#noteFuserTab').addClass('current');
+  hide_other_tabs();
+  $('#notefuser_tab_content').show();
+  load_notes();
+  return false;
+}
+
+function notefuser_template(){
+  template = "<div id=\"notefuser_tab_content\" style=\"display:none\">" +
+    "<div class=\"content_bd\">"+
+      "<div class=\"module profile_module lengthy\">"+
+        "<div class=\"inner\">"+
+          "<div class=\"hd basic_hd\">"+
+            "<div class=\"supplemental\">"+
+              "<img alt=\"Transparent\" class=\"icn add_icn\" src=\"http://assets2.geni.com/images/transparent.gif?1310602807\"> <a href=\"#\" id=\"create_research_log\">Create Research Log</a>"+
+            "</div>"+
+            "<h2>NoteFuser</h2>"+
+            "<ul class=\"horiz_list small strong\">"+
+              "<li>View Notes</li>"+
+            "</ul>"+
+          "</div>"+
+          "<div id=\"notefuser_paged_content\" class=\"basic_bd\">"+
+            "<div id=\"notefuser_alerts\"></div>"+
+            "<div id=\"notefuser_notes\"></div>"+
+          "</div>"+
+        "</div>"+
+      "</div>"+
+    "</div>"+
+  "</div>";
+  return template;
+}
+
+function create_research_log() {
+  $('#notefuser_alerts').html("Loading...");
+  id = get_focused_id();
+  name = $('span[itemprop="name"]').text();
+  name = $.trim(name);
+  console.log(name);
+  vitals = $('table.data_table tr:first td:first').text();  
+  $.post('http://notefuser.herokuapp.com/notes',{id: id, name: name, locality: '', type: 'log'},function(data){
+    $('#notefuser_alerts').html(data);
+    load_notes();
+  });
+  return false;
+}
+
+$('#noteFuser').live('click',load_notefuser_div);
+$('#profile_tabs li:not(li#noteFuserTab) a').live('click',function() {
+  $('#notefuser_tab_content').hide();
+});
+$('#create_research_log').live('click',create_research_log);
+$('.show_evernote').live('click',function(){
+  guid = $(this).attr('rel');
+  $('#note'+guid).toggle();
+  return false;
+});
+create_nf_link();
+setup_tab_views();
